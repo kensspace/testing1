38ee00a9d1aae5bffb499c3bda521eb1a757e1c1
Ken <i070595@users.noreply.github.com>
2016-04-16 02:47:50 +0800

    add comment

--- tests/test_checkFile.py
+++ tests/test_checkFile.py
@@ -9,13 +9,13 @@ class TestCheckFile(unittest.TestCase):
 
     def setUp(self):
         self.check_file1 = CheckFile(os.path.join(current_dir, 'test_countFile.py'))
-        print os.getcwd()
         self.check_file2 = CheckFile("no_path")
         pass
 
     def tearDown(self):
         pass
-
+    
+    #Check exit file and not exit file
     def test_check_file_path(self):
         self.assertTrue(self.check_file1.check_file_path())
         self.assertFalse(self.check_file2.check_file_path())
